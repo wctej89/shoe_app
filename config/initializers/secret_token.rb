743b1b3327b37f06ed49ff7faabbc7dcd4289cd2
wctej89 <wctej89@gmail.com>
2013-01-05 15:19:22 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoeApp::Application.config.secret_token = '82da7c227e0d75e8c855cb97e03b1dc6444dc82b375cf7ce9b66b8285c286e2379377d2f3d77d9c1fc6f0e9013bb52f5a0628b6ba9399f2da1e14bb24d398211'
